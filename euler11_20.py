8fa73c716c621b06927f7add53e22b5af180eb28
Henry Huang <henry@henry-laptop.(none)>
2010-08-08 00:25:05 -0400

    problem 20 solved!

--- euler11_20.py
+++ euler11_20.py
@@ -14,12 +14,20 @@ def p16(n=2**1000):
     '''
     print sum(map(int, list(str(n))))
 
+def p20(n=100):
+    '''Solution to problem 20
+    Again it is a one liner!!! This is really good python!
+    '''
+
+    #n_fac = reduce(lambda x,y: x*y, xrange(1,n))
+    print sum(map(int, list(str(reduce(lambda x,y: x*y, xrange(1,n))))))
+
 def main():
     '''Main program of module euler11_20 
     '''
 
     func_list = {
-	'p16': p16
+        'p16': p16, 'p20': p20
     }
 		    
     if func_list.has_key(sys.argv[1]):
