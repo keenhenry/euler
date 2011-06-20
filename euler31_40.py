7bd612206db42ede2a88379f05b112cf659e193b
Henry Huang <keenhenry1109@gmail.com>
2011-06-20 20:23:14 +0200

    Solved problem 34

--- euler31_40.py
+++ euler31_40.py
@@ -42,6 +42,28 @@ def is_palindrome(n):
     s = str(n) if not isinstance(n, str) else n 
     return s[::-1] == s
 
+def factorial(n):
+    '''A function to calculate the factorial of n.
+    '''
+
+    ans = 1
+    for i in xrange(1, n+1): ans *= i
+    return ans	
+
+def p34(upper=2540160):
+    '''Solution to problem 34 
+    
+    This problem is similar to problem 30. The key is to find the upper bound.
+    upper==2540160==7x9!
+    '''
+   
+    facts = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
+    s = 0    # sum of all numbers which are equal to the sum of factorial of their digits.
+    for i in xrange(100, upper):
+    	sum_of_factorial_of_digits = 0
+	for d in ydigits(i): sum_of_factorial_of_digits += facts[d]
+    	if i == sum_of_factorial_of_digits: s += i
+    print s
 
 def p36(limit=1000000):
     '''Solution to problem 36 
@@ -58,7 +80,7 @@ def main():
     '''
 
     func_list = {
-	'p36': p36 
+	'p34': p34, 'p36': p36 
     }
 		    
     if func_list.has_key(sys.argv[1]):
